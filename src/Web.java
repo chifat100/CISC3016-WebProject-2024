e138f8986cf0762899cf591489d36f271775621c
chifat100 <therevengehunter@gmail.com>
2024-04-25 14:40:24 +0800

    1
    1

--- src/Web.java
+++ src/Web.java
@@ -1,10 +1,18 @@
+<<<<<<< Updated upstream
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+=======
+>>>>>>> Stashed changes
 import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Web {
 
