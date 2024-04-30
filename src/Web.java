c88d56c31b7fd9e7fa81a3f006933e4b467f8596
chifat100 <therevengehunter@gmail.com>
2024-04-30 11:39:49 +0800

    three website content

--- src/Web.java
+++ src/Web.java
@@ -29,12 +29,15 @@ public class Web {
     
     private void fetch() throws IOException {
         Document doc1 = Jsoup.connect(Site1).userAgent(USER_AGENT).get();
+        Document doc2 = Jsoup.connect(Site2).userAgent(USER_AGENT).get();
         Elements elements = doc1.getElementsByClass("news--item-body");
+        Elements elements2 = doc2.getElementsByClass("con");
+        Elements elements3 = doc3.getElementsByClass("conWidth mianConLeft");
 
         String comment = generateComments();
 
         String htmlContent = "<html><head><title>Aggregated Content and Spell Check</title></head><body>";
-        
+
         
         for (Element e : elements) {
             Element date = e.getElementsByClass("news--item-date").first();
@@ -49,6 +52,15 @@ public class Web {
             else
                 htmlContent +="<div>" + date.html() + "</div>" + "<div>" + title.html() + "</div>"  + "<div>" + desc.html() + "</div>";
                 //System.out.printf("%s %s %s%n", date.html(), title.html(), desc.html());
+        }
+
+        for (Element e : elements3) {
+            Element title = e.getElementsByClass("ng-scrop").first();
+            Element text = e.getElementsByClass("home-text").getFirst();
+        }
+
+        for (Element e: elements2) {
+            Elements last = e.getElementsByClass("last");
         }
 
         htmlContent += "<audio controls><source src='sound.wav' type='audio/mpeg'>Your browser does not support the audio element.</audio>";
